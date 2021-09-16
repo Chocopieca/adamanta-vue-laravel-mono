348a4e012c191e7f201d78c0154d50dd996a58b8
MaxymKopyl <m.s.kopyl.kharkov@gmail.com>
2021-09-16 14:39:12 +0300

    Set up vuetify variables.

---
+++ front-end/vuetify.options.js
@@ -0,0 +1,34 @@
+import colors from "vuetify/es5/util/colors";
+
+export default {
+  breakpoint: {
+    thresholds: {
+      xs: 340,
+      sm: 540,
+      md: 960,
+      lg: 1300,
+    },
+    scrollBarWidth: 24,
+  },
+  icons: {},
+  lang: {},
+  theme: {
+    dark: false,
+    options: { customProperties: true },
+    themes: {
+      light: {
+        primary: colors.blue.darken2,
+        accent: colors.grey.darken3,
+        secondary: colors.amber.darken3,
+        info: colors.teal.lighten1,
+        warning: colors.amber.base,
+        error: colors.deepOrange.accent4,
+        success: colors.green.accent3,
+        main_green: "#98CA33",
+        main_blue: "#236AB8",
+        main_black_blue: "#091D34",
+        main_white: "#FFFFFF",
+      }
+    }
+  }
+}
