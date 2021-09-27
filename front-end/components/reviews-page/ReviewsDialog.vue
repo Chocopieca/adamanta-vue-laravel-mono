7be11b60ecd4246969b9a0190994e4f81872d572
MaxymKopyl <m.s.kopyl.kharkov@gmail.com>
2021-09-27 19:09:42 +0300

    Update ReviewsPage and create relative components.

---
+++ front-end/components/reviews-page/ReviewsDialog.vue
@@ -0,0 +1,65 @@
+<template>
+  <Dialog ref="review">
+    <v-card class="pa-md-5" elevation="0">
+      <template slot="progress">
+        <v-progress-linear
+          color="deep-purple"
+          height="10"
+          indeterminate
+        ></v-progress-linear>
+      </template>
+
+      <v-card-title class="d-flex justify-space-between">
+        <div class="d-flex">
+          <v-avatar size="64">
+            <img alt="user" :src="currentReview.avatar">
+          </v-avatar>
+
+          <div class="ml-3">
+            <div :class="vuetifyBreakpoint.xl ? 'size16-weight700' : 'size14-weight700'">{{ currentReview.full_name }}</div>
+            <v-rating
+              :value="currentReview.stars"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+          </div>
+        </div>
+
+        <div class="size14-weight400" :style="`color: ${vuetifyMainGray}`">
+          {{ dateConvertor(currentReview.date) }}
+        </div>
+      </v-card-title>
+
+      <v-card-text>
+        <div class="my-4 size18-weight700">
+          {{ currentReview.title }}
+        </div>
+
+        <div :class="vuetifyBreakpoint.xl ? 'size16-weight400' : 'size14-weight400'">{{ currentReview.content }}</div>
+      </v-card-text>
+    </v-card>
+  </Dialog>
+</template>
+
+<script>
+export default {
+  name: "ReviewsDialog",
+  components: {
+    Dialog: () => import('~~/components/common/Dialog'),
+  },
+  props: {
+    currentReview: {
+      type: Object,
+      default: () => {},
+    }
+  },
+  methods: {
+    openDialog() {
+      this.$refs.review.toggle();
+    },
+  }
+}
+</script>
--- front-end/pages/ReviewsPage.vue
+++ front-end/pages/ReviewsPage.vue
@@ -1,15 +1,57 @@
 <template>
-  <div>
-    ReviewsPage
-  </div>
+  <v-container class="pa-0">
+    <Breadcrumbs :items="breadcrumbs"/>
+    <div class="px-3">
+      <h1 class="mb-5 size20-weight700">Отзывы</h1>
+      <v-row no-gutters class="mb-md-16">
+        <template v-for="(item, index) in reviews">
+          <v-col :key="index" cols="12" md="3">
+            <ReviewCard :item="item" @openDialog="openDialog(item)"/>
+          </v-col>
+        </template>
+      </v-row>
+
+      <div class="mb-md-16">
+        <PopularProducts />
+      </div>
+    </div>
+
+    <ReviewsDialog ref="review" :current-review="currentReview"/>
+  </v-container>
 </template>
 
 <script>
+import MockMixin from "../mixins/MockMixin";
+
 export default {
-  name: "ReviewsPage"
+  name: "ReviewsPage",
+  components: {
+    Breadcrumbs: () => import('~~/components/common/Breadcrumbs'),
+    PopularProducts: () => import('~~/components/home-page/PopularProducts'),
+    ReviewsDialog: () => import('~~/components/reviews-page/ReviewsDialog'),
+    ReviewCard: () => import('~~/components/reviews-page/ReviewCard'),
+  },
+  mixins: [MockMixin],
+  data() {
+    return {
+      breadcrumbs: [
+        {
+          text: 'Главная',
+          href: '/',
+        },
+        {
+          text: 'Отзывы',
+          href: `/reviews`,
+        },
+      ],
+      currentReview: {},
+    }
+  },
+  methods: {
+    openDialog(item) {
+      this.currentReview = item;
+      this.$refs.review.openDialog();
+    },
+  },
 }
 </script>
-
-<style scoped>
-
-</style>
