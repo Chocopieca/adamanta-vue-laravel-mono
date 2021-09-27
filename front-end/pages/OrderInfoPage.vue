f2434d2cef95578ecc89024542625bff3ee85c54
MaxymKopyl <m.s.kopyl.kharkov@gmail.com>
2021-09-27 17:40:46 +0300

    Updated OrderInfoPage component with mock data.

--- front-end/pages/OrderInfoPage.vue
+++ front-end/pages/OrderInfoPage.vue
@@ -1,12 +1,51 @@
 <template>
-  <div>
-    OrderInfoPage
-  </div>
+  <v-container class="pa-0">
+    <Breadcrumbs :items="breadcrumbs"/>
+    <div class="px-3">
+      <h1 class="mb-5">Контакты</h1>
+      <v-row no-gutters>
+        <v-col cols="12" md="6" class="mb-10">
+          <span :style="`color: ${vuetifyMainBlue};`">Доставка</span>
+          <v-divider class="my-3"/>
+          <div class="pr-5 pr-mb-0">
+            {{ order_info.delivery_content }}
+          </div>
+        </v-col>
+        <v-col cols="12" md="6" class="mb-10">
+          <span :style="`color: ${vuetifyMainBlue};`">Оплата</span>
+          <v-divider class="my-3"/>
+          <div class="pr-5 pr-mb-0">
+            {{ order_info.payment_content }}
+          </div>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
 </template>
 
 <script>
+import MockMixin from "../mixins/MockMixin";
+
 export default {
-  name: "OrderInfoPage"
+  name: "OrderInfoPage",
+  mixins: [MockMixin],
+  components: {
+    Breadcrumbs: () => import('~~/components/common/Breadcrumbs'),
+  },
+  data() {
+    return {
+      breadcrumbs: [
+        {
+          text: 'Главная',
+          href: '/',
+        },
+        {
+          text: 'Доставка и оплата',
+          href: `/order_info`,
+        },
+      ]
+    }
+  },
 }
 </script>
 
