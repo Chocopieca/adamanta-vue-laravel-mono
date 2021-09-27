5667b85702b3a056e312cb9c629828df2077b297
MaxymKopyl <m.s.kopyl.kharkov@gmail.com>
2021-09-27 17:20:27 +0300

    Update ContactPage for mobile version.

--- front-end/pages/ContactPage.vue
+++ front-end/pages/ContactPage.vue
@@ -4,19 +4,19 @@
     <div class="px-3">
       <h1 class="mb-5">Контакты</h1>
       <v-row no-gutters>
-        <v-col cols="12" md="2">
+        <v-col cols="12" md="2" class="mb-10">
           <span :style="`color: ${vuetifyMainBlue};`">Телефон</span>
           <v-divider class="my-3"/>
           <template v-for="(number, index) in contacts.numbers">
             <div :key="index">{{ number }}</div>
           </template>
         </v-col>
-        <v-col cols="12" md="3">
+        <v-col cols="12" md="3" class="mb-10">
           <span :style="`color: ${vuetifyMainBlue};`">Адресс</span>
           <v-divider class="my-3"/>
           {{ contacts.address }}
         </v-col>
-        <v-col cols="12" md="2">
+        <v-col cols="12" md="2" class="mb-10">
           <span :style="`color: ${vuetifyMainBlue};`">График работы</span>
           <v-divider class="my-3"/>
           {{ contacts.working_time }}
