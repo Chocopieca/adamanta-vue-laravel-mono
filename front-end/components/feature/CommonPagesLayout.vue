9d9eac57a3ce9888911ac8db7ad73521b7ba53df
MaxymKopyl <m.s.kopyl.kharkov@gmail.com>
2021-09-28 12:40:43 +0300

    Implemented CommonPageLayout and improved pages.

---
+++ front-end/components/feature/CommonPagesLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container class="pa-0">
+
+    <Breadcrumbs :items="breadcrumbs"/>
+
+    <slot></slot>
+
+    <PopularProducts class="mb-md-100 mb-16" />
+
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "CommonPagesLayout",
+  components: {
+    PopularProducts: () => import('~~/components/home-page/PopularProducts'),
+    Breadcrumbs: () => import('~~/components/common/Breadcrumbs'),
+  },
+  props: {
+    breadcrumbs: {
+      type: Array,
+      default: () => [],
+    }
+  }
+}
+</script>
