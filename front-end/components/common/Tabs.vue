3adbc74ab65ff67eb525ba4f5d66bf88f2d75ea6
MaxymKopyl <m.s.kopyl.kharkov@gmail.com>
2021-09-30 11:55:07 +0300

    Create common Tabs component.

---
+++ front-end/components/common/Tabs.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-tabs :color="vuetifyMainBlue">
+      <v-tab v-for="(item, index) in tabs" :key="index" class="size18-weight700">
+        {{ item.title }}
+      </v-tab>
+
+      <slot />
+    </v-tabs>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Tabs",
+  props: {
+    tabs: {
+      type: Array,
+      default: () => []
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
