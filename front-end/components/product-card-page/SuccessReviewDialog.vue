32026bf443de6f46a727b1a11557625612a37c1b
MaxymKopyl <m.s.kopyl.kharkov@gmail.com>
2021-09-30 13:52:11 +0300

    Created SuccessReviewDialog component.

---
+++ front-end/components/product-card-page/SuccessReviewDialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <Dialog ref="dialog">
+    <div class="flex-center flex-column">
+      <v-img contain max-width="446" :src="baseUrl + 'image/successReview.svg'" class="mb-10" />
+      <h3 class="size36-weight700" :style="`color: ${vuetifyMainGreen}`">Спасибо!</h3>
+      <p class="size22-weight400">Ваш комментарий будет добавлен.</p>
+    </div>
+  </Dialog>
+</template>
+
+<script>
+export default {
+  name: "SuccessReviewDialog",
+  components: {
+    Dialog: () => import('~~/components/common/Dialog'),
+  },
+  methods: {
+    openDialog() {
+      this.$refs.dialog.toggle();
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
