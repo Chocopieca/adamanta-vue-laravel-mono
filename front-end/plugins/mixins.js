213586f8250b9e31f3d5723e7bad7d3d4b5514b2
MaxymKopyl <m.s.kopyl.kharkov@gmail.com>
2021-10-11 15:09:55 +0300

    Minor updates

--- front-end/plugins/mixins.js
+++ front-end/plugins/mixins.js
@@ -2,143 +2,167 @@ import Vue from 'vue'
 import { mapGetters } from 'vuex'
 
 Vue.mixin({
-    data () {
-        return {
-            device_type: false,
-            device_mode: false,
-        }
-    },
-    computed: {
-        ...mapGetters([
-            'domRendered',
-            'cabinetActiveTab',
-            'cabinetActiveLibraryTab',
-            'headData',
-            'crumbs',
-            'user',
-            'trans',
-            'languages',
-            'genres',
-            'menu',
-            'settings'
-        ]),
-        getDeviceType() {
-            return this.$root.device_type
-        },
-        getDeviceMode() {
-            return this.$root.device_mode
-        },
-        vuetifyBreakpoint() {
-          return this.$vuetify.breakpoint
-        },
-        vuetifyMainGreen() {
-          return this.$vuetify.theme.themes.light.main_green;
-        },
-        vuetifyMainBlue() {
-          return this.$vuetify.theme.themes.light.main_blue;
-        },
-        vuetifyMainBlack_blue() {
-          return this.$vuetify.theme.themes.light.main_black_blue;
-        },
-        vuetifyMainWhite() {
-          return this.$vuetify.theme.themes.light.main_white;
-        },
-        vuetifyMainGray() {
-          return this.$vuetify.theme.themes.light.main_gray;
-        },
-        vuetifyWarning() {
-          return this.$vuetify.theme.themes.light.warning;
-        },
-        vuetifyError() {
-          return this.$vuetify.theme.themes.light.error;
-        },
-        vuetifyMainBlack() {
-          return this.$vuetify.theme.themes.light.main_black;
-        },
-        vuetifyTransparent() {
-          return '#00000000';
-        },
-        baseUrl() {
-          return this.$env.appBase;
-        },
-    },
-    methods: {
-        $t(label, defLabel = '') {
-            if(this.trans !== undefined && label !== undefined) {
-                const labels = label.split('.')
-                let translation = this.trans
-
-                labels.forEach((val) => {
-                    translation = translation[val] ? translation[val] : false
-                })
-
-                return translation === false ? defLabel : translation
-            }
-
-            return defLabel !== '' ? defLabel : label
-        },
-        notify(msg, opt = {}) {
-            const def = {
-                title: '',
-                dangerouslyUseHTMLString: true,
-                message: '<strong>'+msg+'</strong>',
-                position: 'top-right',
-                type: 'success',
-                duration: 4000
-            }
-
-            this.$notify(Object.assign({}, def, opt))
-        },
-        getCurrentLang() {
-            // this.languages.find(lang => lang.code === this.$lang.getCurrentCode())
-            const lang = false
-            return lang ?? {
-                title: 'Русский',
-                code: 'ru',
-                iso_code: 'ru_RU'
-            }
-        },
-        getCountSlides() {
-            const spans = {
-                large_displays: 6,
-                desktop: 6,
-                tablet_portrait: 3,
-                small_tablet: 2,
-                smart_phone: 2
-            }
-
-            return spans[this.getDeviceType] ? spans[this.getDeviceType] : 1
-        },
-        addParamsToLocation(params) {
-            params = Object.assign(this.$route.query, params)
-            for(const k in params) {
-                if (!params[k]) delete params[k]
-            }
-            params = Object.keys(params)
-              .map(key => {
-                  return (encodeURIComponent(key) + '=' + encodeURIComponent(params[key]))
-              })
-              .join('&')
-
-            history.pushState({}, null, this.$route.path + (params.length ? '?' + params : ''))
-        },
-        dateConvertor(timestamp = new Date) {
-          let month = String(timestamp.getMonth() + 1);
-          let day = String(timestamp.getDate());
-          const year = String(timestamp.getFullYear());
-
-          if (month.length < 2) month = '0' + month;
-          if (day.length < 2) day = '0' + day;
-
-          return `${day}.${month}.${year}`;
-        },
-        textConvertor(text) {
-          const regExp = /\w/ig;
-          const newText = text.replace(regExp, ' ');
-          return `${newText[0].toUpperCase()}${newText.slice(1)}`;
-        },
-        parseDate(date) {
-          return this.$moment(date).format('D MMMM YYYY HH:mm')
-        }
+  data () {
+    return {
+      device_type: false,
+      device_mode: false,
     }
+  },
+  computed: {
+    ...mapGetters([
+      'domRendered',
+      'cabinetActiveTab',
+      'cabinetActiveLibraryTab',
+      'headData',
+      'crumbs',
+      'user',
+      'trans',
+      'languages',
+      'genres',
+      'menu',
+      'settings'
+    ]),
+    getDeviceType() {
+        return this.$root.device_type
+    },
+    getDeviceMode() {
+        return this.$root.device_mode
+    },
+    vuetifyBreakpoint() {
+      return this.$vuetify.breakpoint
+    },
+    vuetifyMainGreen() {
+      return this.$vuetify.theme.themes.light.main_green;
+    },
+    vuetifyMainBlue() {
+      return this.$vuetify.theme.themes.light.main_blue;
+    },
+    vuetifyMainBlack_blue() {
+      return this.$vuetify.theme.themes.light.main_black_blue;
+    },
+    vuetifyMainWhite() {
+      return this.$vuetify.theme.themes.light.main_white;
+    },
+    vuetifyMainGray() {
+      return this.$vuetify.theme.themes.light.main_gray;
+    },
+    vuetifyWarning() {
+      return this.$vuetify.theme.themes.light.warning;
+    },
+    vuetifyError() {
+      return this.$vuetify.theme.themes.light.error;
+    },
+    vuetifyMainBlack() {
+      return this.$vuetify.theme.themes.light.main_black;
+    },
+    vuetifyTransparent() {
+      return '#00000000';
+    },
+    baseUrl() {
+      return this.$env.appBase;
+    },
+  },
+  methods: {
+    $t(label, defLabel = '') {
+      if(this.trans !== undefined && label !== undefined) {
+        const labels = label.split('.')
+        let translation = this.trans
+
+        labels.forEach((val) => {
+          translation = translation[val] ? translation[val] : false
+        })
+
+        return translation === false ? defLabel : translation
+      }
+
+      return defLabel !== '' ? defLabel : label
+    },
+    notify(msg, opt = {}) {
+      const def = {
+        title: '',
+        dangerouslyUseHTMLString: true,
+        message: '<strong>'+msg+'</strong>',
+        position: 'top-right',
+        type: 'success',
+        duration: 4000
+      }
+
+      this.$notify(Object.assign({}, def, opt))
+    },
+    getCurrentLang() {
+      // this.languages.find(lang => lang.code === this.$lang.getCurrentCode())
+      const lang = false
+      return lang ?? {
+        title: 'Русский',
+        code: 'ru',
+        iso_code: 'ru_RU'
+      }
+    },
+    getCountSlides() {
+      const spans = {
+        large_displays: 6,
+        desktop: 6,
+        tablet_portrait: 3,
+        small_tablet: 2,
+        smart_phone: 2
+      }
+
+      return spans[this.getDeviceType] ? spans[this.getDeviceType] : 1
+    },
+    addParamsToLocation(params) {
+      params = Object.assign(this.$route.query, params)
+      for(const k in params) {
+        if (!params[k]) delete params[k]
+      }
+      params = Object.keys(params)
+        .map(key => {
+            return (encodeURIComponent(key) + '=' + encodeURIComponent(params[key]))
+        })
+        .join('&')
+
+      history.pushState({}, null, this.$route.path + (params.length ? '?' + params : ''))
+    },
+    dateConvertor(timestamp = new Date) {
+      let month = String(timestamp.getMonth() + 1);
+      let day = String(timestamp.getDate());
+      const year = String(timestamp.getFullYear());
+
+      if (month.length < 2) month = '0' + month;
+      if (day.length < 2) day = '0' + day;
+
+      return `${day}.${month}.${year}`;
+    },
+    textConvertor(text) {
+      const regExp = /\w/ig;
+      const newText = text.replace(regExp, ' ');
+      return `${newText[0].toUpperCase()}${newText.slice(1)}`;
+    },
+    parseDate(date) {
+      return this.$moment(date).format('D MMMM YYYY HH:mm')
+    },
+    getTitle() {
+      return {
+        category: (arr, link) => this.getCategoryTitle(arr,link),
+        product: (arr, link) => this.getProductTitle(arr,link),
+      }
+    },
+    getCategoryTitle(arr, link) {
+      let title = '';
+      arr.forEach(item => {
+        if(link === item.link) {
+          title = item.title
+        }
+      })
+      return title;
+    },
+    getProductTitle(arr, link) {
+      let title = '';
+      arr.forEach(item => {
+        if(link === item.linkGroup.product) {
+          title = item.title
+        }
+      })
+      return title;
+    },
+  }
 })
